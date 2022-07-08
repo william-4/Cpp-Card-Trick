c874752426415b1c73a291478debb4aeedc94bd1
Liam <93374467+william-4@users.noreply.github.com>
2022-07-08 12:12:06 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,28 +1,18 @@
-/* File: Cardtrick.cpp
- * Programmer:
- * Date:
- * Course: COP 2931
- *
- * Purpose:
- * Write a program that performs a cardtrick. The program will create a
- * random deck of cards, deal them out, pick them up, and determine the
- * secret card.
- *
-*/
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 // Function prototypes
-void BuildDeck( int deck[], const int size);
+void BuildDeck( int deck[], const int size );
+void PrintDeck( int deck[], const int size );
+void PrintCard( int card );
 
 int main(void)
 {
 
 /* declare and initialize variables */
-	int column = 0, i = 0;
-	char name[10] = {};
+	int column = 0, i = 0, c = 0, r = 0;
+	char *name;
 	char answer;
 
 /* Declare a 52 element array of integers to be used as the deck of cards */
@@ -41,34 +31,27 @@ int main(void)
 /* Openning message.  Ask the player for his/her name */
 	cout << "Hello, I am a really tricky computer program and " << endl
 	     << "I can even perform a card trick.  Here's how." << endl
-	     << "To begin the card trick type in your name: " << endl
-	     << " --> ";
+	     << "To begin the card trick type in your name: "<< endl
+	     << "--> ";
 	cin >> name;
 
 /* Capitalize the first letter of the person's name. */
 	name[0] -= 32;
-	cout << name << endl;
-
-
 	cout << endl << "Thank you " << name << endl;
 
+	// do
+	// {
     /* Build the deck */
-	BuildDeck(deck, size);
-
+	BuildDeck(deck, 52);
 
     /* Ask if the player wants to see the entire deck. If so, print it out. */
-	cout << "Ok " <<  name <<", first things first.  Do you want to see what " << endl
-	     << "the deck of cards looks like (y/n)? ";
-	cin >> answer;
-
-	if (answer == 'y')
-		for (i = 0; deck[i]; i++)
-			cout << deck[i] << " " << endl;
-
-
-	cout << endl << name << " pick a card and remember it..." << endl;
-	return 1;
-
+    cout << "Ok " << name << ", first things first.  Do you want to see what " << endl
+		 << "the deck of cards looks like (y/n)? ";
+    cin >> answer;
+    if (answer == 'y')
+        PrintDeck(deck, 52);
+        
+    return 1;
 }
 
 void BuildDeck( int deck[], const int size)
@@ -81,15 +64,76 @@ void BuildDeck( int deck[], const int size)
     {
 		/* generate a random number between 0 and 51 */
 	    card = rand() % 52;
-
-
 		/* Check the used array at the position of the card.
 		   If 0, add the card and set the used location to 1.  If 1, generate another number */
-		if(used[card] == 0)
-		{
-			deck[card] = card;
-			used[card] = 1;
-		}
+		  if(used[card] == 0)
+		  {
+			  deck[card] = card;
+			  used[card] = 1;
+			  /* only increament i if the value is not in used */
+			  i++;
+		  }
     }
+
+}
+void PrintDeck( int deck[], const int size )
+{
+	int i;
+
+	/* Print out each card in the deck */
+	for (i = 0; i < size; i++)
+		PrintCard(deck[i]);
+}
+
+void PrintCard( int card )
+{
+	int rank = 0;
+	int suit = 0;
+	
+	char *rankArray[4] = {"King of ", "Queen of ", "Jack of ", "Ace of "};
+	char *suitArray[4] = {"Spades", "Diamonds", "Clubs", "Hearts"};
+
+	// Determine the rank of the card and print it out i.e. Queen
+	if (card == 10 || card == 23 || card == 36 || card == 49)
+	{
+		cout << rankArray[0];
+	}
+	else if (card == 11 || card == 24 || card == 37 || card == 50)
+	{
+	    cout << rankArray[1];
+	}
+	else if (card == 12 || card == 25 || card == 38 || card == 51)
+	{
+	    cout << rankArray[2];
+	}
+	else if (card == 0 || card == 13 || card == 26|| card == 39)
+	{
+	    cout << rankArray[3];
+	}
+	else
+	{
+	    if (card < 13)
+	        rank = card += 1;
+	    else if (card > 12 && card < 26)
+	        rank = card % 12;
+	    else if (card > 25 && card < 39)
+	        rank = card % 25;
+	    else if (card > 39 && card < 52)
+	        rank = card % 38;
+	    
+	    cout << rank << " of ";
+	}
+	
+	// Determine the suit of the card and print it out i.e. of Clubs
+	if (card < 13)
+	    suit = 0;
+	else if (card > 12 && card < 26)
+	    suit = 1;
+	else if (card > 25 && card < 39)
+	    suit = 2;
+	else if (card > 39 && card < 52)
+	    suit = 3;
+	    
+	cout << suitArray[suit] << endl;
 	return;
 }
